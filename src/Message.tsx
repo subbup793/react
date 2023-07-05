5e707ecb7a9559f1e7efff03860caecc92eeabaf
subbup793 <rocksubbu33@gmail.com>
2023-07-05 23:55:26 +0530

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+ //PascalCasing
+function Message() {
+    //Jsx: JavaScript XML
+    const name = "Subbu";
+    if(name){
+    return <h1>Hello World from {name}</h1>;
+    }
+    else{
+        return <h1>Hello World</h1>;
+    }
+}
+export default Message;
